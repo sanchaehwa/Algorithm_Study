8847ed96e3d54ea5ebac290cae2120fd071e2653
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-01-21 18:18:22 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 22.00 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,44 +1,32 @@
 #t="3141592"
-#p="271" 
-def list_change(number):
-    result = []
-    for i in number:
-        result.append(int(i))
-    return result
-
-def pointer(t, p):
-    t_list = list_change(t)
-    t_len = len(t_list)
-    p_len = len(p)
-    
+#
+def solution(t,p):
+    list_t = [int(char) for char in t]
+    list_p = [int(char) for char in p]
+    #t_len
+    k = len(list_t)
+    #p_len
+    j = len(list_p)
+   
     pointer_list = []
-
     start = 0
-    end = start + p_len - 1
-
-    while end < t_len:
-        number = int("".join(map(str, t_list[start:end + 1])))
-        #t로 만들수 있는 부분수열
+    end = start + j -1
+    #부분문자열
+    while end < k:
+        number = int("".join(map(str,list_t[start:end + 1])))
         pointer_list.append(number)
-
-        # 포인터 이동
         start += 1
         end += 1
+        
+    #print(pointer_list)
+    #부분문자열 중에서도 조건에 맞는걸 answer로
 
-    return pointer_list
-
-def solution(t, p):
-    
-    list_pointer = pointer(t, p)
     values = []
-    #문자열 -> 정수
-    int_p = int(p)    
-    for i in list_pointer:
-            #271보다 작거나 같은가
+    int_p = int(p)
+    for i in pointer_list:
         if i <= int_p:
             values.append(i)
-           # print(values)
-                #그런 경우의 갯수
             answer = len(values)
+            #print(answer)
     return answer
 #print(solution(t,p))
