35afd2982dce68e375bacb7b49284d19bc74989f
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-01-21 18:50:20 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.py
@@ -0,0 +1,11 @@
+#num = 4
+def solution(num):
+    return1 = "Odd"
+    return2 = "Even"
+    if (num % 2 == 1 ):
+       #홀수
+        answer = return1 
+    else:
+        answer = return2
+    return answer
+#print(solution(num))
