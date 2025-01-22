18359ccb0bf9bfce3bb53056b03cc7d3298474c5
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-01-22 14:56:44 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.28 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,47 +1,52 @@
+#순회 하면서, 각 숫자를 n길이만큼 이진수로 변환 array에 담아
+#변환한 걸 또 순회하면서, 문자열로 변환하고 문자열을 answer append
+#map1,2이니깐 함수 호출
+#n = 5
+#arr1 = [9, 20, 28, 18, 11]
+#arr2 = [30, 1, 21, 17, 28]
 # 이진수 변환 함수
-def replace_binarynumber(n, number):
+def replace_binarynumber(number, n):
     binary_array = []
-
     for i in number:
         binary_list = []
         while i != 0:
             binary_list.append(i % 2)
             i = i // 2
         binary_list.reverse()
-        while len(binary_list) < n:
+        while len(binary_list) < n:  # n 길이를 기준으로 왼쪽에 0 채움
             binary_list.insert(0, 0)
         binary_array.append(binary_list)
-
     return binary_array
 
-# 문자 변환 함수 
+# 문자 변환 함수 (각 행을 문자열로 변환)
 def replace_char(binary_array):
     list_char = []
-
     for i in binary_array:
-        s = ""
-        for j in i:
-            if j == 0:
-                s += " "
-            else:
-                s += "#"
+        s = "".join("#" if bit else " " for bit in i)
         list_char.append(s)
-
     return list_char
 
 def solution(n, arr1, arr2):
+    # 이진수 변환
+    replace_binarynumber_arr1 = replace_binarynumber(arr1, n)
+    replace_binarynumber_arr2 = replace_binarynumber(arr2, n)
+
+    # 문자 변환
+    replace_char_arr1 = replace_char(replace_binarynumber_arr1)
+    replace_char_arr2 = replace_char(replace_binarynumber_arr2)
+
+    # 두 배열을 비교해 결과 생성
     result = []
-    replace_binarynumber_arr1 = replace_binarynumber(n, arr1)  # 'n' 추가
-    replace_binarynumber_arr2 = replace_binarynumber(n, arr2)  # 'n' 추가
-
-    for row1, row2 in zip(replace_binarynumber_arr1, replace_binarynumber_arr2):
-        merged_row = ""
-        for bit1, bit2 in zip(row1, row2):
-            if bit1 == 1 or bit2 == 1:
-                merged_row += "#"
+    for row1, row2 in zip(replace_char_arr1, replace_char_arr2):
+        p = ""
+        for char1, char2 in zip(row1, row2):
+            if char1 == "#" or char2 == "#":
+                p += "#"
             else:
-                merged_row += " "
-        result.append(merged_row)
+                p += " "
+        result.append(p)
 
+    #print(result)  
     return result
 
+#print(solution(n, arr1, arr2))
