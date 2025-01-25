c5033ceaefe022d7ebaffe659008087fd2a28f33
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-01-25 11:06:31 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.33 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -4,40 +4,49 @@
 #n = 5
 #arr1 = [9, 20, 28, 18, 11]
 #arr2 = [30, 1, 21, 17, 28]
-# 이진수 변환 함수
-def replace_binarynumber(number, n):
+def replace_binnumber(number, n):
+    # 이진수 변환 
+   # binary_array = []
+    # for i in number:
+    #     binary_list = []
+    #     while i != 0:
+    #         binary_list.append(i % 2)
+    #         i = i // 2
+    #     binary_list.reverse()
+    #     while len(binary_list) < n:  # n 길이를 기준으로 왼쪽에 0 채움
+    #         binary_list.insert(0, 0)
+    #     binary_array.append(binary_list)
     binary_array = []
     for i in number:
-        binary_list = []
-        while i != 0:
-            binary_list.append(i % 2)
-            i = i // 2
-        binary_list.reverse()
-        while len(binary_list) < n:  # n 길이를 기준으로 왼쪽에 0 채움
-            binary_list.insert(0, 0)
+        binary_str = bin(i)[2:]  # '0b' 제거 
+        binary_str = binary_str.zfill(n)  # n 길이를 기준으로 왼쪽에 0 채움
+        binary_list = [int(bit) for bit in binary_str]  # 문자열을 정수 리스트로 변환
         binary_array.append(binary_list)
-    return binary_array
-
-# 문자 변환 함수 (각 행을 문자열로 변환)
-def replace_char(binary_array):
+   # return binary_array
+   #문자 변환 
     list_char = []
     for i in binary_array:
         s = "".join("#" if bit else " " for bit in i)
         list_char.append(s)
     return list_char
+    
+
+# # 문자 변환 함수 (각 행을 문자열로 변환)
+# def replace_char(binary_array):
+#     list_char = []
+#     for i in binary_array:
+#         s = "".join("#" if bit else " " for bit in i)
+#         list_char.append(s)
+#     return list_char
 
 def solution(n, arr1, arr2):
-    # 이진수 변환
-    replace_binarynumber_arr1 = replace_binarynumber(arr1, n)
-    replace_binarynumber_arr2 = replace_binarynumber(arr2, n)
-
-    # 문자 변환
-    replace_char_arr1 = replace_char(replace_binarynumber_arr1)
-    replace_char_arr2 = replace_char(replace_binarynumber_arr2)
+    # 이진수 변환 + 문자 변환
+    replace_binnumber_arr1 = replace_binnumber(arr1, n)
+    replace_binnumber_arr2 = replace_binnumber(arr2, n)
 
     # 두 배열을 비교해 결과 생성
     result = []
-    for row1, row2 in zip(replace_char_arr1, replace_char_arr2):
+    for row1, row2 in zip(replace_binnumber_arr1,  replace_binnumber_arr2 ):
         p = ""
         for char1, char2 in zip(row1, row2):
             if char1 == "#" or char2 == "#":
@@ -46,7 +55,7 @@ def solution(n, arr1, arr2):
                 p += " "
         result.append(p)
 
-    #print(result)  
+   # print(result)  
     return result
-
+ 
 #print(solution(n, arr1, arr2))
