265bdf6c55a2cb3a5d634df9b8288076eb25639e
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-01-28 16:14:36 +0900

    [level 1] Title: 시저 암호, Time: 3.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -2,14 +2,14 @@
 n = 4
 def solution(s, n):
     answer = ''
-    alphabet = [chr(i) for i in range(ord('A'),ord('Z')+1)]
+    alphabet_upper = [chr(i) for i in range(ord('A'),ord('Z')+1)]
     alphabet_lower = [chr(i) for i in range(ord('a'),ord('z')+1)]
     s_split = list(s)
    # print(s_split)
     for idx,i in enumerate(s_split):
         if 'A' <= i <= 'Z':
-            alphabet_index = alphabet.index(i)
-            i = alphabet[(alphabet_index + n) % 26]
+            alphabet_index = alphabet_upper.index(i)
+            i = alphabet_upper[(alphabet_index + n) % 26]
           #  print(i)
         elif 'a' <= i <= 'z':
             alphabet_index = alphabet_lower.index(i)
