4ab9b503813fecb3ff11de44198203e905562e73
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-02-26 15:41:24 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -0,0 +1,11 @@
+#number = "5 5"
+#def solution(number):
+A,B = map(int,input().split())
+    
+if A > B:
+    print('>')
+elif A < B:
+    print('<')
+else:
+    print('==')
+#print(solution(number))
