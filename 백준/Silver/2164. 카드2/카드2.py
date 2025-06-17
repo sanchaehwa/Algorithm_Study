d84791db78bce91965189a1696560350049bcc6c
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-06-17 11:05:40 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 51896 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,59 +1,18 @@
 import sys
-
-class ListNode:
-    def __init__(self, val=None, prev=None, next=None):
-        self.val = val
-        self.prev = prev
-        self.next = next
-
-class Cards:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def append(self, val):
-        new_node = ListNode(val)
-        if not self.head:  # 리스트가 비어있는 경우
-            self.head = self.tail = new_node
-            new_node.next = new_node.prev = new_node
-        else:
-            new_node.prev = self.tail
-            new_node.next = self.head
-            self.tail.next = new_node
-            self.head.prev = new_node
-            self.tail = new_node
-
-    def pop_front(self):
-        if self.head == self.tail:  # 하나만 남은 경우
-            val = self.head.val
-            self.head = self.tail = None
-            return val
-        val = self.head.val
-        self.head = self.head.next
-        self.head.prev = self.tail
-        self.tail.next = self.head
-        return val
-
-    def move_front_to_back(self):
-        if self.head != self.tail:
-            self.head = self.head.next
-            self.tail = self.tail.next
-
-    def get_only_node_value(self):
-        return self.head.val if self.head else None
-
+from collections import deque
+#제일 위에 있는 카드 (First IN) -- 아래로 (First Out) => 큐 사용하는 문제
 class Solution:
-    def card2(self):
+    def card2(self)->int:
+        #N개의 카드 
         n = int(sys.stdin.readline())
-        dq = Cards()
-        for i in range(1, n + 1):
-            dq.append(i)
-
-        while dq.head != dq.tail:
-            dq.pop_front()
-            dq.move_front_to_back()
-
-        print(dq.get_only_node_value())
-
+        #입력값 검증
+        if n<=0:
+            return -1
+        #Queue 
+        queue = deque(range(1,n+1))
+        while len(queue) > 1: #Queue에 하나가 남을때까지 반복
+            queue.popleft() #제일 위에 있는 카드를 버려
+            queue.append(queue.popleft())#그 다음 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮기고
+        return queue[0]
 solution = Solution()
-solution.card2()
+print(solution.card2())
