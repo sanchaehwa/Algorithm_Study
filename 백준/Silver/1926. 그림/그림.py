e84e6db4a3f1d87328dff1d9d1ee81628f842dbf
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-07-10 16:27:26 +0900

    [Silver I] Title: 그림, Time: 300 ms, Memory: 48684 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,32 +1,50 @@
 import sys
-sys.setrecursionlimit(10**6)
- 
-def dfs(x,y):
-    dx = [0,0,-1,1]
-    dy = [-1,1,0,0]
-    cnt = 1 #넓이 
+input = sys.stdin.readline
+'''
+1. 아이디어 
+    - 2중 For 문 1이 나올때마다 && 방문하지않은것
+    - 그림 개수 + 1 / 최대값을 갱신
+    - 시간복잡도 O(v + v4) = O(5v) = *M,N의 최대가 500이니깐 5 * 500 * 500 = 250000 * 5 
+    - E = 4 * (m*n)
+'''
+def bfs(y,x): #y = i, x = j
+    rs = 1 
     visited[y][x] = 1
-    for i in range(4):
-        x1 = x + dx[i]
-        y1 = y + dy[i]
-        if 0<= x1 < m and 0<= y1 < n:
-            if not visited[y1][x1] and picture[y1][x1] == 1:
-                cnt += dfs(x1,y1)
-    return cnt
+    q = [(y,x)]
+    #상하좌우
+    x = [0,0,1,-1]
+    y = [-1,1,0,0]
 
-#세로 -- 가로
-n,m = map(int,sys.stdin.readline().split())
+    #queue에 더이상 들어가지않을떄 
+    while q:
+        ey , ex = q.pop()
+        for k in range(4):
+            dy = ey + y[k]
+            dx = ex + x[k]
+            if 0 <= dy < M and 0 <= dx < N:
+                if visited[dy][dx] == 0 and graph[dy][dx]:
+                    rs += 1
+                    visited[dy][dx] = 1
+                    q.append((dy,dx))
+    return rs
+# M,N 입력
+M,N = map(int,input().split())
+#M * N 만큼 입력
+graph = [list(map(int,input().split())) for _ in range(M)]
+#방문 기록
+visited = [[0] * N for _ in range(M)]
+#2중 For문 1이 나올때마다 방문하지않은것에대한 bfs 수행
+# 전체 그림 갯수
+cnt = 0
+# 그림 크기
+maxV = 0
 
-picture = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
-visited = [[0] * m for _ in range(n)]
-count = []
-for y in range(n):
-    for x in range(m):
-        if visited[y][x] == 0 and picture[y][x] == 1:
-            count.append(dfs(x,y))
-if len(count) == 0:
-    print(len(count))
-    print(0)
-else:
-    print(len(count))
-    print(max(count))
+for i in range(M): #y
+    for j in range(N): #x
+        if graph[i][j] == 1 and visited[i][j] == 0:
+            # 전체 그림 갯수를 + 1
+            # BFS > 그림 크기 구하고 -> 최대값 갱신
+            cnt += 1 
+            maxV = max(maxV, bfs(i,j))
+print(cnt)
+print(maxV)
