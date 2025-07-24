e27ad0e422a1668ccaf3c63cfdf21c48a68a4898
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-07-24 14:19:11 +0900

    [Silver I] Title: 안전 영역, Time: 764 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,48 +1,40 @@
 import sys
-sys.setrecursionlimit(1000000)
+from collections import deque
+input = sys.stdin.readline
+#행과 열의 개수 == N이하 높이는 물의잠김
+def bfs(y,x,height):
+    q = deque()
+    q.append((y,x))
+    while q:
+        ey,ex = q.popleft()
+        dy = [-1,1,0,0]
+        dx = [0,0,1,-1]
+        for i in range(4):
+            ny = dy[i] + ey
+            nx = dx[i] + ex
+            if 0 <= ny < N and 0 <= nx < N:
+                if not visited[ny][nx] and graph[ny][nx] >= height:
+                    q.append((ny,nx))
+                    visited[ny][nx] = True
 
-def dfs(x,y):
-    visited[x][y]=True
-    cnt = 0
-    x1 = [0,0,1,-1]
-    y1 = [-1,1,0,0]
-    for i in range(4):
-        dx = x1[i] + x 
-        dy = y1[i] + y
-        if 0 <= dx < N and 0 <= dy <N:
-            if rain[dx][dy] == 0 and not visited[dx][dy]:
-                dfs(dx,dy)
-
-
-
-#행 과 영의 개수를 나타내는 N
-
-N = int(sys.stdin.readline())
-#아무것도 입력하지않는 경우
+#기준 높이가 6일때, 7일때 최대개수를 구해야하는거니깐
+# N * N
+N = int(input())
+#아무것도 입력하지않는경우
 if N == 0:
     print(0)
     exit()
-
-graph = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
-max_rain = max(map(max,graph))
-#안전영역 list
-safe = []
-current = 0 #현재 위치
-#비에 잠긴 영역을 표시된 graph
-for h in range(max_rain+1):
-    #비의 양 보다 이하인것은 물에 잠기는 영역 - 1
-    rain = [[0]*N for _ in range(N)] #0으로 초기화
+graph = [list(map(int,input().split())) for _ in range(N)]
+result = []
+max_height = max(map(max,graph))
+for h in range(0,max_height+1):
+    height = h
     visited = [[False] * N for _ in range(N)]
-    for x in range(N):
-        for y in range(N):
-            if graph[x][y] <= h:
-                rain[x][y] = 1
-    total = 0
-    for i in range(N):
-        for j in range(N):
-            if rain[i][j] == 0 and visited[i][j] == False:
-                dfs(i,j)
-                total += 1
-    
-    safe.append(total)
-print(max(safe))
+    cnt =0
+    for y in range(N):
+        for x in range(N):
+            if not visited[y][x] and graph[y][x] >= height:
+                bfs(y,x,height)
+                cnt +=1
+    result.append(cnt)
+print(max(result))
