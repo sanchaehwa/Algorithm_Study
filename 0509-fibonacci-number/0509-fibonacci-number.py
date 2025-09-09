4d1f48d98f7d34baecf984380d19e575bf0f0479
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-09-09 22:48:44 +0900

    Time: 44 ms (44.55%), Space: 17.8 MB (23.32%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1 
+        
+        F = [0] * (n+1)
+        F[0], F[1] = 0,1
+        for i in range(2,n+1):
+            F[i] = F[i-1] + F[i-2]
+        return F[n]
