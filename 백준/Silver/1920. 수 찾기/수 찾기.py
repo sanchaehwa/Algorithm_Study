edc72a450d04956ea2b0eaae5662e7f77113cb97
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-11-15 23:10:44 +0900

    [Silver IV] Title: 수 찾기, Time: 404 ms, Memory: 49152 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,41 @@
+'''
+특정 수를 찾아라 ->> 이진 / 이분탐색
+정렬된 배열
+
+'''
 import sys
+
 input = sys.stdin.readline
 
-N = int(input())
-A = sorted(list(map(int,input().split())))
-M = int(input())
-F = list(map(int,input().split()))
-
-for f in F:
-    st = 0
-    en = N-1
-    md = (st+en) // 2
-    flag = False
-    while st <= en:
-        md = (st+en) // 2
-        if A[md] == f:
-            print(1)
-            flag = True
-            break
-        elif A[md] < f:
-            st = md +1
+def find_x(N,aList,target):
+    result = list()
+    for t in target:
+        st = 0
+        en = N-1
+        found = False
+        while st <= en:
+            md = (st+en) // 2
+            if t < aList[md]: #en 옮겨
+                en  = md -1
+            elif t > aList[md]: #st 증가
+                st = md + 1
+            else:
+                found = True
+                break
+        if found:
+            result.append(1)
         else:
-            en = md -1
-    if not flag:
-        print(0)
-
+            result.append(0)
+    return result
     
+
+N  = int(input())
+
+aList = list(map(int,input().split()))
+aList.sort(reverse=False)
+
+M = int(input())
+target = list(map(int,input().split()))
+
+for i in find_x(N,aList,target):
+    print(i)
